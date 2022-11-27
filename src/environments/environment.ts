a91cf14d4bd39627bd46b02a0e1314ea6818cde3
rafael <rafa.rami12.10@gmail.com>
2022-11-27 14:32:37 -0600

    Feat: Se agrego el proyecto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDXsRL3vChsWwGnugw8TtNT4Pl5_s-Cq28",
+    authDomain: "login-b4610.firebaseapp.com",
+    projectId: "login-b4610",
+    storageBucket: "login-b4610.appspot.com",
+    messagingSenderId: "535581890072",
+    appId: "1:535581890072:web:e35aa2528f888ae438556d"
+  }
 };
 
 /*
